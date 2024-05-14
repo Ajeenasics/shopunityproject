253204aa0d5c8152dc4d4b8ba42ffb5d0b01faad
Ajeenasics <ajeena.sics@gmail.com>
2024-05-14 22:48:33 +0530

    register and login done

--- shopunity/src/APIS/axiosinstatnce.js
+++ shopunity/src/APIS/axiosinstatnce.js
@@ -3,10 +3,10 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: "http://localhost:8002",
 
-  headers: {
-    "Content-Type": "multipart/form-data", // Since you're sending form data
-    Authorization: "Bearer YOUR_ACCESS_TOKEN", // Include your authorization token if needed
-  },
+  // headers: {
+  //   "Content-Type": "multipart/form-data", // Since you're sending form data
+  //   Authorization: "Bearer YOUR_ACCESS_TOKEN", // Include your authorization token if needed
+  // },
 });
 
 export default axiosInstance;
